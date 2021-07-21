c1352c9452c63976a1522334e6082fe3b0097476
xuzewei <xuzewei_2012@126.com>
2021-07-21 19:14:27 +0800

    helm chart 2.2.2-r202107211902

--- src/main/java/com/github/microprograms/spring_cloud_starter_kubernetes_demo/controller/CpuController.java
+++ src/main/java/com/github/microprograms/spring_cloud_starter_kubernetes_demo/controller/CpuController.java
@@ -2,8 +2,10 @@ package com.github.microprograms.spring_cloud_starter_kubernetes_demo.controller
 
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/cpu")
 public class CpuController {
-    private ExecutorService executorService = Executors.newSingleThreadExecutor();
+    private ExecutorService executorService = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS,
+            new SynchronousQueue<Runnable>()); // 容量为1的线程池
 
     @RequestMapping("/startCalc")
+
     public String startCalc() {
         try {
-            executorService.execute(() -> {
+            executorService.submit(() -> {
                 while (!Thread.interrupted()) {
                     while (true) {
-                        Math.pow(Math.PI, new Random().nextDouble());
+                        System.out.println(Math.pow(Math.PI, new Random().nextDouble()));
                     }
                 }
             });
